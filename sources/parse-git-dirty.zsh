c786a1e78ed28237fd9a0530c397a23da507d1c6
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-09-09 19:08:06 +0200

    Source a few helpers.

---
+++ sources/parse-git-dirty.zsh
@@ -0,0 +1,13 @@
+
+# Checks if working tree is dirty
+function parse_git_dirty() {
+  local SUBMODULE_SYNTAX=''
+  if [[ $POST_1_7_2_GIT -gt 0 ]]; then
+        SUBMODULE_SYNTAX="--ignore-submodules=dirty"
+  fi
+  if [[ -n $(git status -s ${SUBMODULE_SYNTAX}  2> /dev/null) ]]; then
+    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+  else
+    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+  fi
+}
